7b7a83217196224dee08d9bac3ff68f769fcb2fb
za59418 <za59418@8c1f1bd6-ee13-4fdb-94fb-ff93a6243388>
2018-01-22 09:24:41 +0000

    上传文件和发送邮件

--- trunk/TestSpringBoot/src/main/java/com/zxl/service/IUserService.java
+++ trunk/TestSpringBoot/src/main/java/com/zxl/service/IUserService.java
@@ -7,4 +7,8 @@ import com.zxl.domain.User;
 public interface IUserService {
 	User getUser(String name);
 	List<User> getAllUsers();
+	void save(User user);
+	User findUserById(Long id);
+	void edit(User user);
+	void delete(Long id);
 }
